eef2b5317a2f8523d47f00411d18f735c53240c6
SidharthR20 <sidharthbuntirout@gmail.com>
2023-08-15 22:18:51 +0530

    Initial Commit: Rendering in-progress page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,277 @@
-import Image from 'next/image'
-
+var flag = false;
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  if (flag) {
+    return (
+      <>
+        <header>
+          <nav>
+            <div className="left">Sidharth's Portfolio</div>
+            <div className="right">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#about-me">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                {/* <!-- <li><a href="#footer-main">Contact Me</a></li> --> */}
+                {/* <li>
+                <button
+                  className="nav-btn"
+                  onclick={() => {
+                    window.scroll({
+                      top: document.body.scrollHeight,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  Contact Me
+                </button>
+              </li> */}
+              </ul>
+            </div>
+            {/* <script>
+            {function handleScroll() {
+              window.scroll({
+                top: document.body.scrollHeight,
+                behavior: "smooth",
+              });
+            }}
+          </script> */}
+          </nav>
+        </header>
+        <main>
+          <section className="firstSection">
+            <div className="leftSection">
+              <div>Hi, My name is</div>
+              <span className="purple-username">Sidharth</span>
+              <div>and I am a passionate</div>
+              <span id="element"></span>
+              {/* <div className="buttons">
+              <button
+                type="button"
+                className="btn"
+                onclick={() => {
+                  window.open(
+                    "resources/Documents/Sidharth-Rout-Resume-updated.pdf"
+                  );
+                }}
+              >
+                Download Resume
+              </button>
+              <button
+                type="submit"
+                className="btn"
+                onclick={() => {
+                  window.open("https://github.com/SidharthR20");
+                }}
+              >
+                Visit Github
+              </button> */}
+              {/* </div> */}
+              {/* {function handleDownloadResume() {
+              window.open(
+                "resources/Documents/Sidharth-Rout-Resume-updated.pdf"
+              );
+            }}
+            {function handleGitHubRedirection() {
+              window.open("https://github.com/SidharthR20");
+            }} */}
+            </div>
+            <div className="rightSection">
+              <img
+                src="./Images/web-dev-at-work.png"
+                alt="web-developer-at-work"
+              />
+            </div>
+          </section>
+
+          <hr />
+
+          <section className="secondSection" id="about-me">
+            <h1>Overall Journey</h1>
+            <span className="grey-text">What I have done so far</span>
+            <div className="box">
+              <div className="vertical">
+                <img
+                  className="box-top-image"
+                  src="./Images/SSE-img.png"
+                  alt=""
+                />
+                <div className="vertical-title">
+                  Senior Secondary <br />
+                  (2013-2015)
+                </div>
+                <div className="vertical-description">
+                  Hi, please write something related to Senior Secondary
+                  experience.
+                </div>
+              </div>
+              <div className="vertical">
+                <img
+                  className="box-top-image"
+                  src="./Images/UG-degree.png"
+                  alt=""
+                />
+                <div className="vertical-title">
+                  B.Tech in CSE <br />
+                  (2015-2019)
+                </div>
+                <div className="vertical-description">
+                  Hi, please write something related to React developer
+                  experience.
+                </div>
+              </div>
+              <div className="vertical">
+                <img
+                  className="box-top-image"
+                  src="./Images/dev-at-work.png"
+                  alt=""
+                />
+                <div className="vertical-title">
+                  Cobol Developer <br />
+                  (2019-2021)
+                </div>
+                <div className="vertical-description">
+                  Hi, please write something related to Cobol developer
+                  experience.
+                </div>
+              </div>
+              <div className="vertical">
+                <img
+                  className="box-top-image"
+                  src="./Images/developer.png"
+                  alt=""
+                />
+                <div className="vertical-title">
+                  React Developer <br />
+                  (2021-present)
+                </div>
+                <div className="vertical-description">
+                  Hi, please write something related to React developer
+                  experience.
+                </div>
+              </div>
+
+              {/* <!-- <div class="vertical"></div> --> */}
+            </div>
+          </section>
+        </main>
+        <footer id="footer-main">
+          <div className="footer">
+            <div className="footer-first">
+              <h3>Sidharth's Developer Portfolio</h3>
+            </div>
+            <div className="footer-second">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="footer-third">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+            <div className="footer-fourth">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-rights">
+            Copyright &#169; sidharthsportfolio.com | All rights reserved
+          </div>
+        </footer>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header>
+          <nav>
+            <div className="left">Sidharth's Portfolio</div>
+            <div className="right">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <div
+            style={{
+              height: "84vh",
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "whitesmoke",
+              alignItems: "center",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <h1
+              style={{
+                textAlign: "center",
+                color: "black",
+                marginTop: "226px",
+              }}
+            >
+              This page is in progress currently.
+              <br />
+              Thank you for visiting.
+            </h1>
+          </div>
+        </main>
+        <footer>
+          {/* <div className="footer">
+            <div className="footer-first">
+              <h3>Sidharth's Developer Portfolio</h3>
+            </div>
+            <div className="footer-second">
+              <ul>
+                <li>Home</li>
+              </ul>
+            </div>
+            <div className="footer-third">
+              <ul>
+                <li>Home</li>
+              </ul>
+            </div>
+            <div className="footer-fourth">
+              <ul>
+                <li>Home</li>
+              </ul>
+            </div>
+          </div> */}
+          <div className="footer-rights">
+            Copyright &#169; sidharthsportfolio.com | All rights reserved
+          </div>
+        </footer>
+      </>
+    );
+  }
 }
